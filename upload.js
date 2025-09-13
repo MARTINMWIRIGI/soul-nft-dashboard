3dfa320e475229b406828e9974b149f0415e7387
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-09-13 15:20:20 +0300

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,36 @@
+import { NFTStorage, File } from 'nft.storage';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const busboy = await import('busboy');
+    const bb = busboy.default({ headers: req.headers });
+    const chunks = [];
+
+    bb.on('file', (_, file, info) => {
+      file.on('data', (d) => chunks.push(d));
+      file.on('end', () => {});
+    });
+
+    bb.on('close', async () => {
+      const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
+      const fileBuffer = Buffer.concat(chunks);
+      const file = new File([fileBuffer], 'upload.png', { type: 'image/png' });
+      const cid = await nftstorage.storeBlob(file);
+      res.status(200).json({ cid: cid.toString() });
+    });
+
+    req.pipe(bb);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
